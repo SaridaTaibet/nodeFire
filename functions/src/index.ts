d6540e05a2c44b1d4150061bd21538bf205212c7
SaridaTaibet <taibetsarida@hotmail.fr>
2019-06-24 18:52:00 +0200

    Node Express

--- functions/src/index.ts
+++ functions/src/index.ts
@@ -31,16 +31,16 @@ let hostels = [
 
 const app = express();
 
-/*app.get('/read', (req, res) => {
+app.get('/read', (req, res) => {
     res.send(hostels).status(200)
-});*/
+});
 
 
-/*app.get('/read/:id', (req, res) => {
+app.get('/read/:id', (req, res) => {
     res.send(hostels[req.params.id]).status(200)
-});*/
+});
 
-/*app.post('/create', (req, res) => {
+app.post('/create', (req, res) => {
     hostels.push(req.body);
     res.send(hostels).status(201)
 });
@@ -49,7 +49,7 @@ app.put('/update/:id', (req, res) => {
     const hostel = hostels.filter(value => value.id !== parseInt(req.params.id));
     hostel.push(req.body);
     res.send(hostel).status(200);
-});*/
+});
 
 app.patch('/update1/:id', (req, res) => {
     const [hostel] = hostels.filter(value => value.id === parseInt(req.params.id));
@@ -60,29 +60,11 @@ app.patch('/update1/:id', (req, res) => {
 });
 
 
-
-   /* let newhostel = Object.assign(hostel, data);
-    res.send(newhostel).status(200);*/
-/*});*/
-
-/*app.patch('/hostels/:id', (req, res) => {
-    const hostel = hostels.filter(value => value.id !== parseInt(req.params.id));
-    const modif = req.body;
-    let nHostel = Object.assign(hostel, modif).lenght;
-    res.send(nHostel).status(200);
-});*/
-
-/*const hostel1 = hostels.filter(value => value.id !== parseInt(req.params.id));
-const hostel2 = hostel1.push(req.body);*/
-
-
-/*hostel2.concat(hostel1);*/
-
-
 app.delete('/delete/:id', (req, res) => {
     const newhostels = hostels.filter(value => value.id !== parseInt(req.params.id));
     res.send(newhostels).status(200)
 });
+
 
 app.delete('/delete', (req, res) => {
     hostels = [];
